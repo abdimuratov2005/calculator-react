abca02a6c4a8309091a44d21ab67987adb63fdf2
Abdimuratov Abdulla <deshark550@gmail.com>
2023-03-14 15:30:28 +0500

    fix: optimized, scss-module

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,81 @@
 import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './sass/style.module.scss'
+
+// ==========================================================//
 
 const App = () => {
     const [result, setResult] = useState('');
     const numbers = [];
+    const operations = [];
+
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
         .forEach(num => {
             numbers.push(
-                <Button variant="primary" type="submit" onClick={e => {
+                <Button type="button" onClick={e => {
                     setResult(result + e.target.value)
-                }} value={num} key={num} 
+                }} value={num} key={num}
                 >
-                    { num }
+                    {num}
                 </Button>
             )
         });
+
+    ['+', '-', '*', '/']
+        .forEach(oper => {
+            operations.push(
+                <Button type="button" onClick={e => {
+                    setResult(result + e.target.value)
+                }} value={oper} key={oper}
+                >
+                    {oper}
+                </Button>
+            )
+        });
+
     const reset = () => {
         setResult('')
     }
+    const allResult = () => {
+        try {
+            setResult(
+                String(eval(result).lenght > 3 &&
+                    String(eval(result)).includes('.')
+                    ? String(eval(result).toFixed(4))
+                    : String(eval(result))),
+                setResult(result.toString())
+            )
+        } catch (error) {
+            alert(error)
+        }
+    }
+
+// ==========================================================//
+
     return (
-        <div id="app">
-            <div className="app__body calculator">
-                <div className="calculator__model">
+        <div className={styles.app}>
+            <div className={styles.calculator}>
+                <div>
                     <Form.Control
-                        className='result'
+                        className={styles.result}
                         type='text'
                         placeholder="0"
                         value={result}
                         onChange={event => setResult(event.target.value)}
                     />
                 </div>
-                <div className='calculator__btns'>
-                    { numbers }
+                <div className={styles.btns}>
+                    {numbers}
+                    {operations}
                     <Button onClick={reset}>R</Button>
-                    <Button value='+' onClick={e => setResult(result + e.target.value)}>+</Button>
-                    <Button value='-' onClick={e => setResult(result + e.target.value)}>-</Button>
-                    <Button value='*' onClick={e => setResult(result + e.target.value)}>*</Button>
-                    <Button value='/' onClick={e => setResult(result + e.target.value)}>/</Button>
-                    <Button value='=' onClick={e => {
-                        try {
-                            setResult(
-                                String(eval(result).lenght > 3 &&
-                                String(eval(result)).includes('.')
-                                ? String(eval(result).toFixed(4))
-                                : String(eval(result))),
-                                setResult(result.toString())
-                            )
-                        } catch (error){
-                            alert(error)
-                        }
-                    }}>=</Button>
+                    <Button className={styles.allresult} value='=' onClick={allResult}>=</Button>
                 </div>
             </div>
         </div>
     )
 }
+
+// ==========================================================//
 
 export default App;
